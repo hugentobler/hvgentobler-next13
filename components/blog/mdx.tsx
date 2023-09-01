c0f322cac76e737d1d78cc2121857275fd301ac7
Christopher Hugentobler <hugentoblerc@gmail.com>
2023-09-01 15:25:46 -0700

    Added more blog styling

--- components/blog/mdx.tsx
+++ components/blog/mdx.tsx
@@ -12,7 +12,7 @@ const customComponents = {
     </h2>
   ),
   h3: (props: any) => (
-    <h3 {...props} className="bg-background mt-6 mb-3 text-xl font-light tracking-tight leading-tight ">
+    <h3 {...props} className="bg-background mt-6 mb-3 text-xl font-light tracking-tight leading-tight break-after-avoid">
       {props.children}
     </h3>
   ),
@@ -22,12 +22,17 @@ const customComponents = {
     </p>
   ),
   ol: (props: any) => (
-    <ol {...props} className="bg-background list-decimal list-inside break-inside-avoid-column">
+    <ol {...props} className="bg-background mb-3 list-decimal list-inside space-y-1 break-before-avoid">
       {props.children}
     </ol>
   ),
+  ul: (props: any) => (
+    <ul {...props} className="bg-background mb-3 list-disc list-inside space-y-1 break-before-avoid">
+      {props.children}
+    </ul>
+  ),
   li: (props: any) => (
-    <li {...props} className="bg-background mb-3 text-sm leading-5">
+    <li {...props} className="bg-background text-sm leading-5">
       {props.children}
     </li>
   ),
@@ -35,6 +40,11 @@ const customComponents = {
     <DecoratedLink href={props.href} target="_blank">
       {props.children}
     </DecoratedLink>
+  ),
+  blockquote: (props: any) => (
+    <blockquote className="bg-background [&_p]:text-2xl [&_p]:italic font-light tracking-tight [&_p]:leading-7 my-6">
+      {props.children}
+    </blockquote>
   ),
   img: async function(props: any) {
     const { base64, height, width } = await ImageProperties(props.src)
