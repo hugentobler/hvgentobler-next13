361f64bbdfc5d5d6c53377ca8120283c8200aa38
Christopher Hugentobler <hugentoblerc@gmail.com>
2023-09-27 15:59:00 -0700

    Experimenting with locations grid

--- components/home-page/locations.tsx
+++ components/home-page/locations.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 import Image from 'next/image'
 import { locations } from '@/data/locations'
 
@@ -29,83 +29,53 @@ export default function Locations() {
         <h3 className="
         text-lg lg:text-base font-light
         col-start-1 col-span-1">Travels</h3>
-        <h3 className="col-start-2 col-span-1">Time</h3>
-        <h3 className="col-start-5 col-span-2">Place</h3>
+        {/* <h3 className="col-start-2 col-span-1">Time</h3>
+        <h3 className="col-start-5 col-span-2">Place</h3> */}
       </header >
-      <ul className="divide-y divide-zinc-150 border-t border-zinc-200">
+      <ul className="divide-y divide-neutral-200 border-y">
         {locations.map(({ time, place, content }, i) => {
           const city = place.split(',')[0]
           const hash = city.toLowerCase().replace(/\s/g, '-') + '-' + time.toLowerCase().replace(/\s/g, '-')
           return (
-            <li key={i} id={hash} className="group relative">
+            <li key={i} id={hash} className="group relative target:scroll-mt-20">
               {/* Input 'name' groups all radio buttons. Setting a constant here allows radio buttons to be toggled without javascript */}
               <input type="radio" name="loc" id={`input-${i}`} value={hash}
                 className={`peer appearance-none ${content ? "cursor-pointer" : ""} absolute w-full h-full`
                 }
-                defaultChecked={i === 0}
+                defaultChecked={i == 0}
                 disabled={content ? false : true}
-                onClick={() => (setTimeout(() => { location.href = `#${hash}` }, 400))}
+                onClick={() => (setTimeout(() => { location.href = `#${hash}` }, 250))}
               />
               <label htmlFor={`input-${i}`} className="
-              grid grid-cols-11 gap-x-3 md:gap-x-8
+              grid grid-cols-12 gap-x-6 justify-between
               py-3 text-sm text-secondary leading-4
-              peer-enabled:peer-hover:text-primary peer-enabled:peer-hover:bg-background
-              peer-checked:text-primary peer-checked:bg-zinc-50
+              peer-enabled:peer-hover:text-primary peer-enabled:peer-hover:bg-neutral-50
+              peer-checked:text-primary peer-checked:bg-neutral-50
               ">
-                <div className="col-span-3 col-start-2">
-                  <p>{time}</p>
-                </div>
-                <div className="col-span-7 col-start-5">
+                <div className="col-span-6 justify-self-end">
                   <p>{place}</p>
+                </div>
+                <div className="col-span-6">
+                  <p>{time}</p>
                 </div>
               </label>
               {content &&
                 <div className="
-                grid grid-cols-11 gap-x-3 md:gap-x-8
-                py-3 text-sm
-                h-0 peer-checked:h-auto
+                grid grid-flow-row-dense grid-cols-12 gap-1
+                text-sm h-0 peer-checked:h-auto peer-checked:pb-3
                 scale-y-0 peer-checked:scale-y-100 origin-top
+                peer-checked:bg-neutral-50
                 ">
-                  <div className="col-span-7 col-start-5
-                    first-letter:text-6xl first-letter:float-left first-letter:mr-2 first-letter:mt-1 first-letter:-mb-6 first-letter:font-extralight first-letter:leading-none
+                  <div className="col-span-7 col-start-5 px-3
+                    first-letter:text-6xl first-letter:float-left first-letter:mr-1 first-letter:mt-1 first-letter:-mb-4 first-letter:font-extralight first-letter:leading-none
                     ">
                     {content.text}
                   </div>
                   {content.images &&
-                    <div className="col-span-4 col-start-1">
-                      {content.images.map((img, i) => (
-                        <Image
-                          key={i}
-                          className="col-span-2"
-                          alt=''
-                          src={img}
-                          width={500}
-                          height={250}
-                        />
-                      ))}
-                    </div>
-                  }
-                </div>
-              }
-              {/* {content &&
-                <div className="grid grid-cols-7 gap-x-6
-                h-0 peer-checked:h-auto
-                bg-zinc-50 text-xs
-                py-0 peer-checked:py-1
-                opacity-0 peer-checked:opacity-100
-                scale-y-0 peer-checked:scale-y-100 origin-top
-                transition-none duration-200 ease-linear peer-checked:transition"
-                >
-                  {content.text &&
-                    <>
-                      <p className="col-start-4 col-span-2">{content.text}</p>
-                    </>
-                  }
-                  {content.images &&
                     content.images.map((img, i) => (
                       <Image
                         key={i}
-                        className="col-span-2"
+                        className="col-span-4"
                         alt=''
                         src={img}
                         width={500}
@@ -114,7 +84,7 @@ export default function Locations() {
                     ))
                   }
                 </div>
-              } */}
+              }
             </li>
           )
         })}
